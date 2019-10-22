f7706c70e687bf10b9b94e2487b9dfe08428d404
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-22 19:32:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,6 +13,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)[:get_emoticon][emoticon]
+  load_library(file_path)[:get_emoticon][emoticon] ? load_library(file_path)[:get_emoticon][emoticon] : "Sorry, "
 end
 
 def get_english_meaning
